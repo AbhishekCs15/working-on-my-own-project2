12764956586255fa4b60088989f2c441b503b005
S Abhishek <s.abhishek1715@gmail.com>
2025-03-14 21:48:37 +0530

    2nd stage

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template, redirect,url_for
+from flask import Flask, render_template, redirect,url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer,String,VARCHAR
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_registration.db'
+# app.config['SQLALCHEMY_Binds'] = {
+#     'db2': 'sqlite:///user_details .db',
+# }
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email:Mapped[str] = mapped_column(VARCHAR,unique=True)
+    password:Mapped[str] = mapped_column(VARCHAR)
+    name:Mapped[str] = mapped_column(String)
+    phone:Mapped[str] = mapped_column(Integer, unique=True)
+
+with app.app_context():
+    db.create_all()
+
 
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
@@ -17,8 +42,32 @@ class LoginForm(FlaskForm):
 def register():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return redirect(url_for('s'))
+        user = User(
+            email=login_form.email.data,
+            name=login_form.name.data,
+            password=login_form.password.data,
+            phone=login_form.phone.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template("register.html", form=login_form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        email = login_form.email.data
+        password = login_form.password.data
+        phone = login_form.phone.data
+        # print(email)
+        # print(password)
+        # print(phone)
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+
+        if user.password == password and user.phone == phone and user.email == email:
+            return redirect(url_for('s'))
+    return render_template('login.html', form=login_form)
 
 @app.route('/h',methods=['GET','POST'])
 def s():
